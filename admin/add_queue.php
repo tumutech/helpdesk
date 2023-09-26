75f6b56ed204b2a3baf42afdf56aaa7bd7ee005f
tumutech <ftumusiime73@gmail.com>
2023-09-26 11:40:29 +0300

    v5

--- admin/add_queue.php
+++ admin/add_queue.php
@@ -4,10 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add Queue</title>
+    <?php 
+    include('admin_template.php');
+    ?>
 
 </head>
 <body>
-    <h5>Add Queue</h5>
+    <div class="container-fluid">
+<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+<h5>Add Queue</h5>
+    <div class="row">
     <form>
         <label>* Name:</label><input type="text">
         <br>
@@ -15,7 +21,7 @@
         <br>
         <label> * Follow up Option: </label><input type="text">
         <br>
-        <label> * Ticket lock after follow up: </label><input type="text">
+        <label> * Ticket lock after follow up: </label><input type="text"><br>
         <label> * System address: </label><input type="text">
         <br>
         <label> * Salutation: </label><input type="text">
@@ -24,8 +30,10 @@
         <br>
         <label> * Validity: </label><input type="text">
         <br>
-        <input type="submit" value="Save"> or <a href="#">Cancel</a>
+        <input type="submit" value="Save"> or <a href="queues.php">Cancel</a>
     </form>
-    
+</div>
+</main>
+</div>
 </body>
 </html>
